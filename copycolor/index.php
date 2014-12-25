5fed1fb1e2e0091e8f4fdb7095b3d261b6ff51e8
hannahbe <hannah.bellaiche@gmail.com>
2014-12-25 15:56:28 +0200

    main page design
    changes so the design adapts to screen size
    index.php
    style.css

--- copycolor/index.php
+++ copycolor/index.php
@@ -5,17 +5,17 @@ get_header(); ?>
 
     <div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/mainbanner.png" alt="A mug with your own design" width="1349px">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/mainbanner.png" alt="A mug with your own design" width="100%">
         </div><!-- #content -->
         <div id="content2" class="site-content" role="main">
             <div id="print" class="pds">
-                <img src="<?php echo get_stylesheet_directory_uri() ?>/images/print.png" alt="Print">
+                <img src="<?php echo get_stylesheet_directory_uri() ?>/images/print.png" alt="Print" width="60%">
             </div><!-- #print -->
             <div id="design" class="pds">
-                <img src="<?php echo get_stylesheet_directory_uri() ?>/images/design.png" alt="Design">
+                <img src="<?php echo get_stylesheet_directory_uri() ?>/images/design.png" alt="Design" width="60%">
             </div><!-- #design -->
             <div id="sublimation" class="pds">
-                <img src="<?php echo get_stylesheet_directory_uri() ?>/images/sublimation.png" alt="Sublimation">
+                <img src="<?php echo get_stylesheet_directory_uri() ?>/images/sublimation.png" alt="Sublimation" width="60%">
             </div><!-- #sublimation -->
         </div><!-- #content2 -->
 	</div><!-- #primary -->
